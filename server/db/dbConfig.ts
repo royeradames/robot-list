25a030b644e6ba34257329c38a6af664e479ecfe
royeradames <royeraadames@gmail.com>
2021-12-09 18:15:55 -0500

    👌remove template code

--- server/db/dbConfig.ts
+++
@@ -1,7 +0,0 @@
-/* import libraries*/
-const knex = require("knex");
-const knexConfig = require("../knexfile");
-
-const enviroment = process.env.DB_ENV || "development";
-
-module.exports = knex(knexConfig[enviroment]);
