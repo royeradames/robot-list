16c1a4d651cef1506bd7415d25a8e8450c7f9d27
royeradames <royeraadames@gmail.com>
2021-12-09 18:05:14 -0500

    📦 add student type

--- server/api/schema.ts
+++ server/api/schema.ts
@@ -5,5 +5,24 @@ const typeDefs = gql`
     "List all students"
     students: [Students!]!
   }
+  "A student"
+  type Students {
+    id: ID!
+    "The city of the student"
+    city: String
+    "The company of the student"
+    company: String
+    "The email of the student"
+    email: String
+    "The firstName of the student"
+    firstName: String
+    "The student's grade"
+    grades: [String]
+    "The lastName of the student"
+    lastName: String
+    "The student's picture"
+    pic: String
+    "The student's skill"
+    skill: String
 `;
 export default typeDefs;
