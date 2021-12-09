8c9b1737d39030e720a3ec7fc44247425d78724d
royeradames <royeraadames@gmail.com>
2021-12-09 17:45:53 -0500

    📦install libraries

--- react-front-end/src/index.tsx
+++ react-front-end/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./sass/index.scss";
-import App from "./App";
+import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
