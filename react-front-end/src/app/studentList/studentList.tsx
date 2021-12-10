1811a759c2c90607e4c046f3d9d271468840601e
royeradames <royeraadames@gmail.com>
2021-12-10 14:43:49 -0500

    📦filter students by input name

--- react-front-end/src/app/studentList/studentList.tsx
+++ react-front-end/src/app/studentList/studentList.tsx
@@ -39,6 +39,27 @@ export default function StudentList(): JSX.Element {
   const watchName = watch("name");
 
   const { error, data } = useQuery(GET_STUDENTS);
+  const filterByName = (watchName: string | undefined) => {
+    /*
+      filter the list with the watchName value
+      - if watchName is empty, show all the list
+      - if watchName is not empty, filter the list with the watchName value
+        - if the watchName value is in the firstName or lastName, show the student
+    */
+    if (watchName) {
+      const filterStudent = data.students.filter((student: StudentType) => {
+        const name = `${student.firstName.toLowerCase()} ${student.lastName.toLowerCase()}`;
+        return name.includes(watchName);
+      });
+      return filterStudent.map((student: StudentType) => {
+        return <Student key={student.id} student={student} />;
+      });
+    } else {
+      return data.students.map((student: StudentType) => {
+        return <Student key={student.id} student={student} />;
+      });
+    }
+  };
 
   // handle error
   if (error) <ErrorHandler error={error} />;
@@ -47,7 +68,7 @@ export default function StudentList(): JSX.Element {
     return (
       <article className={styles["student-list"]}>
         <input {...register("name")} placeholder="Search by Name" />
-        {loadStudents(watchName)}
+        {filterByName(watchName)}
       </article>
     );
   }
