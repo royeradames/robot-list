8062494e1e214b0d4acc4522994b42d478acaf99
royeradames <royeraadames@gmail.com>
2021-12-13 15:23:44 -0500

    📦add a general error message

--- react-front-end/src/component/errorHandler/errorHandler.tsx
+++ react-front-end/src/component/errorHandler/errorHandler.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./errorHandler.module.scss";
-import { ApolloError } from "@apollo/client";
 
-export default function ErrorHandler({ error }: { error: ApolloError }) {
+export default function ErrorHandler({ error }: { error: any }) {
   return (
     <article className={styles.error}>
       <h1 className={styles["error-title"]}>Error</h1>
-      <p className={styles["error-message"]}>{error.message}</p>
+      <p className={styles["error-message"]}>
+        There was an error while getting the student information. Please try,
+        again.
+      </p>
     </article>
   );
 }
